aeb8ac1f1d77c0bd36d07f0dee36341d8fd7cad5
DVLK <32389666+DVLK@users.noreply.github.com>
2020-12-03 19:35:15 +0100

    error handling

--- scripts/flashlight.js
+++ scripts/flashlight.js
@@ -10,25 +10,33 @@ navigator.mediaDevices
     // get the active track of the stream
     track = stream.getVideoTracks()[0];
   })
-  .catch((err) => console.error("getUserMedia() failed: ", err));
+  .catch((err) => document.getElementById("errors").innerHTML += "getUserMedia(): " + err + "\n--------------------------\n");
 
 function setTorch(value) {
-  if (track.getCapabilities().torch) {
-    track
-      .applyConstraints({
-        advanced: [{ torch: value }],
-      })
-      .catch((e) => console.log(e));
-  }
-}
-
-function toggleTorch() {
-    console.log("toggle")
+  try{
     if (track.getCapabilities().torch) {
       track
         .applyConstraints({
-          advanced: [{ torch: !track.getConstraints()["torch"] }],
+          advanced: [{ torch: value }],
         })
         .catch((e) => console.log(e));
+    }
+  }catch(err){
+    document.getElementById("errors").innerHTML += "setTorch(): " + err + "\n--------------------------\n"
+  }
+  
+}
+
+function toggleTorch() {
+    try{
+      if (track.getCapabilities().torch) {
+        track
+          .applyConstraints({
+            advanced: [{ torch: !track.getConstraints()["torch"] }],
+          })
+          .catch((e) => console.log(e));
+      }
+    }catch(err){
+      document.getElementById("errors").innerHTML += "setTorch(): " + err + "\n--------------------------\n"
     }
   }
